004b8c7019c6461db4210762cca2084e4245ee82
datns <nsdat1802@gmail.com>
2020-09-01 20:42:25 +0700

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,27 @@
+const sgMail = require('@sendgrid/mail');
+const sender= "nsdat1802@gmail.com";
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+
+const sendWelcomeEmail = (email, name) => {
+	sgMail.send({
+		to: email,
+		from: sender,
+		subject: "Thanks for joining in!",
+		text: `Welcome to the app, ${name}. Let me know how you get along with the app.`
+	})
+};
+
+const sendCancelationEmail = (email, name) => {
+	sgMail.send({
+		to: email,
+		from: sender,
+		subject: "Sorry to see you go!",
+		text: `Goodbye, ${name}. I hope to see you back sometime soon.`
+	})
+};
+
+module.exports = {
+	sendWelcomeEmail,
+	sendCancelationEmail
+};
